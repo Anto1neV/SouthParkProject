25f371db803ed6e6da0e1cd7550ddd7c0d97f82b
Kylian <kylian.salomon@isen.yncrea.fr>
2020-10-09 16:06:44 +0200

    Add :
    + Character.java
    + Gang.java (enum)
    + Position.java

---
+++ .history/SouthPark/src/Character_20201009160531.java
@@ -0,0 +1,8 @@
+import java.util.List;
+
+abstract class Character {
+    protected List<String> wordList;
+    protected int size;
+    protected Gang gang;
+    protected Position position;
+}
---
+++ SouthPark/src/Gang.java
@@ -0,0 +1,4 @@
+public enum Gang {
+    WestGang,
+    EastGang
+}
